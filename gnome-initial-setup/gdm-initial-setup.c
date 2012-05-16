893311ce4c6dab799c744f262a8661a2bea343ec
Jasper St. Pierre <jstpierre@mecheye.net>
2012-05-16 14:00:03 -0400

    initial-setup: Hook up "Determine location automatically" button
    Get the longitude/latitude from geoclue, and then feed it to GWeather,
    which can find the information.

--- gnome-initial-setup/gdm-initial-setup.c
+++ gnome-initial-setup/gdm-initial-setup.c
@@ -37,6 +37,9 @@
 #include <cheese-gtk.h>
 #endif
 
+#include <geoclue/geoclue-master.h>
+#include <geoclue/geoclue-position.h>
+
 #include <gnome-keyring.h>
 
 #define DEFAULT_TZ "Europe/London"
@@ -1487,19 +1490,11 @@ location_changed_cb (CcTimezoneMap *map,
 }
 
 static void
-location_changed (GObject *object, GParamSpec *param, SetupData *setup)
+set_location_from_gweather_location (SetupData        *setup,
+                                     GWeatherLocation *gloc)
 {
-        GWeatherLocationEntry *entry = GWEATHER_LOCATION_ENTRY (object);
-        GWeatherLocation *gloc;
-        GWeatherTimezone *zone;
-        gchar *city;
-
-        gloc = gweather_location_entry_get_location (entry);
-        if (gloc == NULL)
-                return;
-
-        zone = gweather_location_get_timezone (gloc);
-        city = gweather_location_get_city_name (gloc);
+        GWeatherTimezone *zone = gweather_location_get_timezone (gloc);
+        gchar *city = gweather_location_get_city_name (gloc);
 
         if (zone != NULL) {
                 const gchar *name;
@@ -1526,7 +1521,84 @@ location_changed (GObject *object, GParamSpec *param, SetupData *setup)
         }
 
         g_free (city);
+}
+
+static void
+location_changed (GObject *object, GParamSpec *param, SetupData *setup)
+{
+        GWeatherLocationEntry *entry = GWEATHER_LOCATION_ENTRY (object);
+        GWeatherLocation *gloc;
+
+        gloc = gweather_location_entry_get_location (entry);
+        if (gloc == NULL)
+                return;
+
+        set_location_from_gweather_location (setup, gloc);
+
         gweather_location_unref (gloc);
+}
+
+static void
+position_callback (GeocluePosition      *pos,
+		   GeocluePositionFields fields,
+		   int                   timestamp,
+		   double                latitude,
+		   double                longitude,
+		   double                altitude,
+		   GeoclueAccuracy      *accuracy,
+		   GError               *error,
+		   SetupData            *setup)
+{
+	if (error) {
+		g_printerr ("Error getting position: %s\n", error->message);
+		g_error_free (error);
+	} else {
+		if (fields & GEOCLUE_POSITION_FIELDS_LATITUDE &&
+		    fields & GEOCLUE_POSITION_FIELDS_LONGITUDE) {
+                        GWeatherLocation *city = gweather_location_find_nearest_city (latitude, longitude);
+                        set_location_from_gweather_location (setup, city);
+		} else {
+			g_print ("Position not available.\n");
+		}
+	}
+}
+
+static void
+determine_location (GtkWidget *widget,
+                    SetupData *setup)
+{
+	GeoclueMaster *master;
+	GeoclueMasterClient *client;
+	GeocluePosition *position = NULL;
+        GError *error = NULL;
+
+	master = geoclue_master_get_default ();
+	client = geoclue_master_create_client (master, NULL, NULL);
+	g_object_unref (master);
+
+	if (!geoclue_master_client_set_requirements (client, 
+	                                             GEOCLUE_ACCURACY_LEVEL_LOCALITY,
+	                                             0, TRUE,
+	                                             GEOCLUE_RESOURCE_ALL,
+	                                             NULL)){
+		g_printerr ("Setting requirements failed");
+                goto out;
+	}
+
+	position = geoclue_master_client_create_position (client, &error);
+	if (position == NULL) {
+		g_warning ("Creating GeocluePosition failed: %s", error->message);
+                goto out;
+	}
+
+	geoclue_position_get_position_async (position,
+	                                     (GeocluePositionCallback) position_callback,
+	                                     setup);
+
+ out:
+        g_clear_error (&error);
+        g_object_unref (client);
+        g_object_unref (position);
 }
 
 static void
@@ -1588,6 +1660,9 @@ prepare_location_page (SetupData *setup)
 
         g_signal_connect (setup->map, "location-changed",
                           G_CALLBACK (location_changed_cb), setup);
+
+        g_signal_connect (WID ("location-auto-button"), "clicked",
+                          G_CALLBACK (determine_location), setup);
 }
 
 /* Online accounts page {{{1 */
