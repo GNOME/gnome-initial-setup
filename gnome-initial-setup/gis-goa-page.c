1a49fdeedd07eb48e10f54b5631a69aa35e8bff4
Jasper St. Pierre <jstpierre@mecheye.net>
2012-06-12 11:38:29 -0400

    goa-page: Import new add-account-dialog from g-c-c

--- gnome-initial-setup/gis-goa-page.c
+++ gnome-initial-setup/gis-goa-page.c
@@ -4,6 +4,7 @@
 
 #include "config.h"
 #include "gis-goa-page.h"
+#include "cc-online-accounts-add-account-dialog.h"
 
 #include <glib/gi18n.h>
 #include <gio/gio.h>
@@ -16,88 +17,52 @@ struct _GoaData {
   GoaClient *goa_client;
 };
 
-static GtkWidget *
-create_provider_button (const gchar *type, const gchar *name, GIcon *icon)
-{
-  GtkWidget *button;
-  GtkWidget *box;
-  GtkWidget *image;
-  GtkWidget *label;
-
-  button = gtk_button_new ();
-
-  box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 6);
-  gtk_widget_set_halign (box, GTK_ALIGN_START);
-  gtk_widget_set_valign (box, GTK_ALIGN_CENTER);
-  gtk_container_add (GTK_CONTAINER (button), box);
-
-  image = gtk_image_new_from_gicon (icon, GTK_ICON_SIZE_BUTTON);
-  gtk_box_pack_start (GTK_BOX (box), image, FALSE, FALSE, 0);
-
-  label = gtk_label_new (name);
-  gtk_box_pack_start (GTK_BOX (box), label, FALSE, FALSE, 0);
-
-  gtk_widget_show (box);
-  gtk_widget_show (image);
-  gtk_widget_show (label);
-
-  g_object_set_data (G_OBJECT (button), "provider-type", (gpointer)type);
-
-  return button;
-}
-
 static void
-add_account (GtkButton *button, gpointer user_data)
+show_online_account_dialog (GtkButton *button,
+                            gpointer   user_data)
 {
   GoaData *data = user_data;
+  GtkWindow *parent;
   GtkWidget *dialog;
-  GtkWidget *goa_dialog;
-  GtkWidget *vbox;
-  const gchar *provider_type;
-  GoaProvider *provider;
+  gint response;
+  GList *providers;
+  GList *l;
+  GoaObject *object;
   GError *error;
-  SetupData *setup = data->setup;
 
-  dialog = WID("online-accounts-dialog");
-  gtk_widget_hide (dialog);
+  providers = NULL;
 
-  provider_type = g_object_get_data (G_OBJECT (button), "provider-type");
+  parent = GTK_WINDOW (gis_get_assistant (data->setup));
 
-  g_debug ("Adding online account: %s", provider_type);
+  dialog = goa_panel_add_account_dialog_new (data->goa_client);
+  gtk_window_set_transient_for (GTK_WINDOW (dialog), parent);
 
-  provider = goa_provider_get_for_provider_type (provider_type);
+  providers = goa_provider_get_all ();
+  for (l = providers; l != NULL; l = l->next)
+    {
+      GoaProvider *provider;
 
-  goa_dialog = gtk_dialog_new ();
+      provider = GOA_PROVIDER (l->data);
+      goa_panel_add_account_dialog_add_provider (GOA_PANEL_ADD_ACCOUNT_DIALOG (dialog), provider);
+    }
 
-  gtk_container_set_border_width (GTK_CONTAINER (goa_dialog), 12);
-  gtk_window_set_modal (GTK_WINDOW (goa_dialog), TRUE);
-  gtk_window_set_resizable (GTK_WINDOW (goa_dialog), TRUE);
-  gtk_window_set_transient_for (GTK_WINDOW (goa_dialog), GTK_WINDOW (gis_get_assistant (setup)));
-  /* translators: This is the title of the "Add Account" dialogue.
-   * The title is not visible when using GNOME Shell
-   */
-  gtk_window_set_title (GTK_WINDOW (goa_dialog), _("Add Account"));
-  gtk_dialog_add_button (GTK_DIALOG (goa_dialog),
-                         GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL);
-
-  vbox = gtk_dialog_get_content_area (GTK_DIALOG (goa_dialog));
-  gtk_widget_set_vexpand (vbox, TRUE);
-
-  gtk_widget_show_all (goa_dialog);
-  gtk_window_present (GTK_WINDOW (goa_dialog));
+  gtk_widget_show_all (dialog);
+  response = gtk_dialog_run (GTK_DIALOG (dialog));
+  if (response != GTK_RESPONSE_OK)
+    {
+      gtk_widget_destroy (dialog);
+      goto out;
+    }
 
   error = NULL;
-  goa_provider_add_account (provider,
-                            data->goa_client,
-                            GTK_DIALOG (goa_dialog),
-                            GTK_BOX (vbox),
-                            &error);
-  gtk_widget_destroy (goa_dialog);
+  object = goa_panel_add_account_dialog_get_account (GOA_PANEL_ADD_ACCOUNT_DIALOG (dialog), &error);
 
-  if (error &&
-      !(error->domain == GOA_ERROR && error->code == GOA_ERROR_DIALOG_DISMISSED))
+  if (object == NULL)
+    {
+      gtk_widget_destroy (dialog);
+      if (!(error->domain == GOA_ERROR && error->code == GOA_ERROR_DIALOG_DISMISSED))
         {
-          dialog = gtk_message_dialog_new (GTK_WINDOW (gis_get_assistant (setup)),
+          dialog = gtk_message_dialog_new (parent,
                                            GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
                                            GTK_MESSAGE_ERROR,
                                            GTK_BUTTONS_CLOSE,
@@ -105,57 +70,16 @@ add_account (GtkButton *button, gpointer user_data)
           gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (dialog),
                                                     "%s",
                                                     error->message);
-          gtk_widget_show (dialog);
+          gtk_widget_show_all (dialog);
           gtk_dialog_run (GTK_DIALOG (dialog));
           gtk_widget_destroy (dialog);
-    }
-}
-
-static void
-populate_online_account_dialog (GoaData *data)
-{
-  GtkWidget *dialog;
-  GtkWidget *content_area;
-  GList *providers, *l;
-  GoaProvider *provider;
-  gchar *provider_name;
-  const gchar *provider_type;
-  GIcon *provider_icon;
-  GtkWidget *button;
-  SetupData *setup = data->setup;
-
-  dialog = WID("online-accounts-dialog");
-  content_area = gtk_dialog_get_content_area (GTK_DIALOG (dialog));
-
-  providers = goa_provider_get_all ();
-  for (l = providers; l; l = l->next)
-    {
-      provider = GOA_PROVIDER (l->data);
-      provider_type = goa_provider_get_provider_type (provider);
-      provider_name = goa_provider_get_provider_name (provider, NULL);
-      provider_icon = goa_provider_get_provider_icon (provider, NULL);
-      button = create_provider_button (provider_type, provider_name, provider_icon);
-      gtk_container_add (GTK_CONTAINER (content_area), button);
-      gtk_widget_show (button);
-      g_free (provider_name);
-
-      g_signal_connect (button, "clicked", G_CALLBACK (add_account), setup);
+        }
+      g_error_free (error);
     }
 
-  g_signal_connect (dialog, "delete-event",
-                    G_CALLBACK (gtk_widget_hide_on_delete), NULL);
-}
-
-static void
-show_online_account_dialog (GtkButton *button, gpointer user_data)
-{
-  GoaData *data = user_data;
-  SetupData *setup = data->setup;
-  GtkWidget *dialog;
-
-  dialog = WID("online-accounts-dialog");
-
-  gtk_window_present (GTK_WINDOW (dialog));
+ out:
+  g_list_foreach (providers, (GFunc) g_object_unref, NULL);
+  g_list_free (providers);
 }
 
 static void
@@ -353,7 +277,6 @@ gis_prepare_online_page (SetupData *setup)
        return;
     }
 
-  populate_online_account_dialog (data);
   populate_account_list (data);
 
   button = WID("online-add-button");
