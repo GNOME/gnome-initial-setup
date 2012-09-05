2b5550527435c299a1063b8176a8483d011bab68
Debarshi Ray <debarshir@gnome.org>
2012-09-05 15:39:22 +0200

    Fix and update legal boilerplate
    Some files written by Matthias Clasen (mclasen) were licensed under
    GPLv3+. He has agreed to relicense them to GPLv2+:
    <mclasen> rishi: ok, you're right, pw-utils is mine, fine to change

--- gnome-initial-setup/pages/language/gdm-languages.h
+++ gnome-initial-setup/pages/language/gdm-languages.h
@@ -3,10 +3,10 @@
  * Copyright 2008 Red Hat, Inc.
  * Copyright 2007 William Jon McCann <mccann@jhu.edu>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
  *
  * Written by: Ray Strode
  *             William Jon McCann
