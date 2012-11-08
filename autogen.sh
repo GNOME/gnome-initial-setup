0923efec801b1e75052e6a090a2f3ba73c9a2269
Colin Walters <walters@verbum.org>
2012-11-08 18:34:58 -0500

    autogen: Flesh out more with standard boilerplate
    Trying to exec this will fail since it has no shebang line.  Also, we
    might as well copy the boilerplate.

--- autogen.sh
+++ autogen.sh
@@ -1,1 +1,24 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+REQUIRED_AUTOMAKE_VERSION=1.7
+USE_GNOME2_MACROS=1
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="gnome-initial-setup"
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/gnome-initial-setup.doap) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level gnome-settings-daemon directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common"
+    exit 1
+}
+
 . gnome-autogen.sh
