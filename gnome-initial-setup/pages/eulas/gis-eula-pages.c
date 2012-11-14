b6ecea57e2847ba1b912e96a2debf671db6e600f
Jasper St. Pierre <jstpierre@mecheye.net>
2012-11-14 17:01:20 -0500

    eulas: Remove horizontal scrolling from the EULA display

--- gnome-initial-setup/pages/eulas/gis-eula-pages.c
+++ gnome-initial-setup/pages/eulas/gis-eula-pages.c
@@ -240,6 +240,9 @@ gis_eula_page_constructed (GObject *object)
   scrolled_window = gtk_scrolled_window_new (NULL, NULL);
   gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scrolled_window),
                                        GTK_SHADOW_ETCHED_IN);
+  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window),
+                                  GTK_POLICY_NEVER,
+                                  GTK_POLICY_AUTOMATIC);
   gtk_widget_set_vexpand (scrolled_window, TRUE);
   gtk_container_add (GTK_CONTAINER (scrolled_window), text_view);
   gtk_container_add (GTK_CONTAINER (vbox), scrolled_window);
