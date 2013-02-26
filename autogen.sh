6cefa137a9032a6c7e6596f095622b7f16567f7a
Jasper St. Pierre <jstpierre@mecheye.net>
2013-02-26 17:43:46 -0500

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-REQUIRED_AUTOMAKE_VERSION=1.7
-USE_GNOME2_MACROS=1
-
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
@@ -16,21 +13,16 @@ PKG_NAME="gnome-initial-setup"
     exit 1
 }
 
-# Fetch submodules if needed
-if test ! -f egg-list-box/COPYING;
-then
-  echo "+ Setting up submodules"
-  git submodule init
-fi
-git submodule update
-
-cd egg-list-box
-sh autogen.sh --no-configure
-cd ..
-
 which gnome-autogen.sh || {
     echo "You need to install gnome-common"
     exit 1
 }
 
+git submodule update --init --recursive
+
+cd egg-list-box
+sh autogen.sh --no-configure
+cd ..
+
+REQUIRED_AUTOMAKE_VERSION=1.7
 . gnome-autogen.sh
