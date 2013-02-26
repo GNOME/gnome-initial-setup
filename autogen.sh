3e6942cfe2f03594562fa153da5612967e415d9c
Jasper St. Pierre <jstpierre@mecheye.net>
2013-02-26 17:26:54 -0500

    Add libgd as a submodule

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="gnome-initial-setup"
+ACLOCAL_FLAGS="-I libgd $ACLOCAL_FLAGS"
 
 (test -f $srcdir/configure.ac \
   && test -f $srcdir/gnome-initial-setup.doap) || {
