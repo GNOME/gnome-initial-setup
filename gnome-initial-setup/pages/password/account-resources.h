a5ef600e38c67bf230ead4518bdb782ca143e5bd
Victor Ibragimov <victor.ibragimov@gmail.com>
2014-02-04 11:48:38 +0500

    Tajik translation updated

--- gnome-initial-setup/pages/password/account-resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_account_H__
-#define __RESOURCE_account_H__
-
-#include <gio/gio.h>
-
-extern GResource *account_get_resource (void);
-#endif
--- gnome-initial-setup/pages/password/password-resources.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_password_H__
-#define __RESOURCE_password_H__
-
-#include <gio/gio.h>
-
-extern GResource *password_get_resource (void);
-#endif
