4aa9db01c8cc4f6462be80c57c5705e38f1fe987
Matthias Clasen <mclasen@redhat.com>
2015-05-16 16:58:05 -0400

    password: Don't allow empty passwords
    Not having a password causes ugly corner cases in many places like
    gnome-keyring and gdm, so don't allow it.

--- gnome-initial-setup/pages/password/gis-password-page.c
+++ gnome-initial-setup/pages/password/gis-password-page.c
@@ -132,9 +132,10 @@ validate (GisPasswordPage *page)
     clear_entry_validation_error (GTK_ENTRY (priv->password_entry));
 
   gtk_label_set_label (GTK_LABEL (priv->confirm_explanation), "");
+  priv->valid_confirm = FALSE;
 
-  priv->valid_confirm = (strcmp (password, verify) == 0);
   if (strlen (password) > 0 && strlen (verify) > 0) {
+    priv->valid_confirm = (strcmp (password, verify) == 0);
     if (!priv->valid_confirm) {
       gtk_label_set_label (GTK_LABEL (priv->confirm_explanation), _("The passwords do not match."));
     }
