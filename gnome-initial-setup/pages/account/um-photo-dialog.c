b3d639a153d269a0381b97b2e8650bce79a9c62a
Ondrej Holy <oholy@redhat.com>
2018-04-09 09:10:45 +0200

    account: Do not show legacy face images
    Legacy face images should not be shown in avatar chooser, so let's filter
    all symlinks to the legacy files out.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=795086

--- gnome-initial-setup/pages/account/um-photo-dialog.c
+++ gnome-initial-setup/pages/account/um-photo-dialog.c
@@ -175,6 +175,8 @@ setup_photo_popup (UmPhotoDialog *um)
         GFile *dir;
         GFileInfo *info;
         GFileEnumerator *enumerator;
+        GFileType type;
+        const gchar *target;
         GtkWidget *menu, *menuitem, *image;
         guint x, y;
         const gchar * const * dirs;
@@ -212,6 +214,19 @@ setup_photo_popup (UmPhotoDialog *um)
                         char *filename;
 
                         added_faces = TRUE;
+
+                        type = g_file_info_get_file_type (info);
+                        if (type != G_FILE_TYPE_REGULAR &&
+                            type != G_FILE_TYPE_SYMBOLIC_LINK) {
+                                g_object_unref (info);
+                                continue;
+                        }
+
+                        target = g_file_info_get_symlink_target (info);
+                        if (target != NULL && g_str_has_prefix (target , "legacy/")) {
+                                g_object_unref (info);
+                                continue;
+                        }
 
                         filename = g_build_filename (path, g_file_info_get_name (info), NULL);
                         menuitem = menu_item_for_filename (um, filename);
