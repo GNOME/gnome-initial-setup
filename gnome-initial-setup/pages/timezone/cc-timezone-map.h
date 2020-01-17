e371c058fe8824cabe54f6f8002a95d5db64ca29
Will Thompson <will@willthompson.co.uk>
2020-01-17 17:14:35 +0000

    timezone: update files from gnome-control-center
    These were last synced in 2016. This updates various bits of timezone
    data and corresponding images, and squashes many warnings when building
    with more recent GLib.

--- gnome-initial-setup/pages/timezone/cc-timezone-map.h
+++ gnome-initial-setup/pages/timezone/cc-timezone-map.h
@@ -18,54 +18,15 @@
  *
  */
 
-
-#ifndef _CC_TIMEZONE_MAP_H
-#define _CC_TIMEZONE_MAP_H
+#pragma once
 
 #include <gtk/gtk.h>
 #include "tz.h"
 
 G_BEGIN_DECLS
 
-#define CC_TYPE_TIMEZONE_MAP cc_timezone_map_get_type()
-
-#define CC_TIMEZONE_MAP(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
-  CC_TYPE_TIMEZONE_MAP, CcTimezoneMap))
-
-#define CC_TIMEZONE_MAP_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST ((klass), \
-  CC_TYPE_TIMEZONE_MAP, CcTimezoneMapClass))
-
-#define CC_IS_TIMEZONE_MAP(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
-  CC_TYPE_TIMEZONE_MAP))
-
-#define CC_IS_TIMEZONE_MAP_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE ((klass), \
-  CC_TYPE_TIMEZONE_MAP))
-
-#define CC_TIMEZONE_MAP_GET_CLASS(obj) \
-  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
-  CC_TYPE_TIMEZONE_MAP, CcTimezoneMapClass))
-
-typedef struct _CcTimezoneMap CcTimezoneMap;
-typedef struct _CcTimezoneMapClass CcTimezoneMapClass;
-typedef struct _CcTimezoneMapPrivate CcTimezoneMapPrivate;
-
-struct _CcTimezoneMap
-{
-  GtkWidget parent;
-
-  CcTimezoneMapPrivate *priv;
-};
-
-struct _CcTimezoneMapClass
-{
-  GtkWidgetClass parent_class;
-};
-
-GType cc_timezone_map_get_type (void) G_GNUC_CONST;
+#define CC_TYPE_TIMEZONE_MAP (cc_timezone_map_get_type ())
+G_DECLARE_FINAL_TYPE (CcTimezoneMap, cc_timezone_map, CC, TIMEZONE_MAP, GtkWidget)
 
 CcTimezoneMap *cc_timezone_map_new (void);
 
@@ -76,5 +37,3 @@ void cc_timezone_map_set_bubble_text (CcTimezoneMap *map,
 TzLocation * cc_timezone_map_get_location (CcTimezoneMap *map);
 
 G_END_DECLS
-
-#endif /* _CC_TIMEZONE_MAP_H */
