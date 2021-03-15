8a116282b7b69f31b15412c8a7108d4cdb08c2d4
Mohammed Sadiq <sadiq@sadiqpk.org>
2021-03-15 09:41:12 +0530

    driver: Port to use G_DECLARE_DERIVABLE_TYPE
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-initial-setup/-/merge_requests/115>

--- gnome-initial-setup/gis-driver.h
+++ gnome-initial-setup/gis-driver.h
@@ -29,26 +29,15 @@
 
 G_BEGIN_DECLS
 
-#define GIS_TYPE_DRIVER               (gis_driver_get_type ())
-#define GIS_DRIVER(obj)                           (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIS_TYPE_DRIVER, GisDriver))
-#define GIS_DRIVER_CLASS(klass)                   (G_TYPE_CHECK_CLASS_CAST ((klass),  GIS_TYPE_DRIVER, GisDriverClass))
-#define GIS_IS_DRIVER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIS_TYPE_DRIVER))
-#define GIS_IS_DRIVER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  GIS_TYPE_DRIVER))
-#define GIS_DRIVER_GET_CLASS(obj)                 (G_TYPE_INSTANCE_GET_CLASS ((obj),  GIS_TYPE_DRIVER, GisDriverClass))
+#define GIS_TYPE_DRIVER (gis_driver_get_type ())
 
-typedef struct _GisDriver        GisDriver;
-typedef struct _GisDriverClass   GisDriverClass;
+G_DECLARE_DERIVABLE_TYPE (GisDriver, gis_driver, GIS, DRIVER, GtkApplication)
 
 typedef enum {
   UM_LOCAL,
   UM_ENTERPRISE,
   NUM_MODES,
 } UmAccountMode;
-
-struct _GisDriver
-{
-  GtkApplication parent;
-};
 
 struct _GisDriverClass
 {
@@ -62,8 +51,6 @@ typedef enum {
   GIS_DRIVER_MODE_NEW_USER,
   GIS_DRIVER_MODE_EXISTING_USER,
 } GisDriverMode;
-
-GType gis_driver_get_type (void);
 
 GisAssistant *gis_driver_get_assistant (GisDriver *driver);
 
