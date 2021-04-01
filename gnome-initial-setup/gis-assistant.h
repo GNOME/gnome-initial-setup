f9527f9fcb847107d6160b44612120e08bc4f497
Mohammed Sadiq <sadiq@sadiqpk.org>
2021-04-01 07:05:03 +0530

    assistant: Port to use G_DECLARE_DERIVABLE_TYPE
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-initial-setup/-/merge_requests/117>

--- gnome-initial-setup/gis-assistant.h
+++ gnome-initial-setup/gis-assistant.h
@@ -27,26 +27,13 @@
 G_BEGIN_DECLS
 
 #define GIS_TYPE_ASSISTANT               (gis_assistant_get_type ())
-#define GIS_ASSISTANT(obj)                           (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIS_TYPE_ASSISTANT, GisAssistant))
-#define GIS_ASSISTANT_CLASS(klass)                   (G_TYPE_CHECK_CLASS_CAST ((klass),  GIS_TYPE_ASSISTANT, GisAssistantClass))
-#define GIS_IS_ASSISTANT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIS_TYPE_ASSISTANT))
-#define GIS_IS_ASSISTANT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  GIS_TYPE_ASSISTANT))
-#define GIS_ASSISTANT_GET_CLASS(obj)                 (G_TYPE_INSTANCE_GET_CLASS ((obj),  GIS_TYPE_ASSISTANT, GisAssistantClass))
 
-typedef struct _GisAssistant        GisAssistant;
-typedef struct _GisAssistantClass   GisAssistantClass;
-
-struct _GisAssistant
-{
-  GtkBox parent;
-};
+G_DECLARE_DERIVABLE_TYPE (GisAssistant, gis_assistant, GIS, ASSISTANT, GtkBox)
 
 struct _GisAssistantClass
 {
   GtkBoxClass parent_class;
 };
-
-GType gis_assistant_get_type (void);
 
 void      gis_assistant_add_page          (GisAssistant *assistant,
                                            GisPage      *page);
