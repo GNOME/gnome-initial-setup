GNOME/gnome-initial-setup

commit a7dfea203494c285cd222f44cf05e9e07bfd6b38

timezone: Reintroduce GisLocationEntry
https://gitlab.gnome.org/GNOME/gnome-initial-setup/-/issues/132

Part-of: <https://gitlab.gnome.org/GNOME/gnome-initial-setup/-/merge_requests/133>

gnome-initial-setup/pages/timezone/gis-timezone-page.c
@@ -49,6 +49,7 @@
 #include "gis-bubble-widget.h"
 
 #include "gis-page-header.h"
+#include "gis-location-entry.h"
 
 #define DEFAULT_TZ "Europe/London"
 #define DESKTOP_ID "gnome-datetime-panel"
@@ -228,15 +229,14 @@ entry_text_changed (GtkEditable *editable,
   priv->search_entry_text_changed_id = 0;
 }
 
-#if 0
 static void
 entry_location_changed (GObject *object, GParamSpec *param, GisTimezonePage *page)
 {
   GisTimezonePagePrivate *priv = gis_timezone_page_get_instance_private (page);
-  GWeatherLocationEntry *entry = GWEATHER_LOCATION_ENTRY (object);
+  GisLocationEntry *entry = GIS_LOCATION_ENTRY (object);
   g_autoptr(GWeatherLocation) location = NULL;
 
-  location = gweather_location_entry_get_location (entry);
+  location = gis_location_entry_get_location (entry);
   if (!location)
     return;
 
@@ -244,7 +244,6 @@ entry_location_changed (GObject *object, GParamSpec *param, GisTimezonePage *pag
   set_location (page, location);
   priv->in_search = FALSE;
 }
-#endif
 
 #define GETTEXT_PACKAGE_TIMEZONES "gnome-control-center-2.0-timezones"
 
@@ -446,10 +445,8 @@ gis_timezone_page_constructed (GObject *object)
   priv->search_entry_text_changed_id =
       g_signal_connect (priv->search_entry, "changed",
                         G_CALLBACK (entry_text_changed), page);
-#if 0
   g_signal_connect (priv->search_entry, "notify::location",
                     G_CALLBACK (entry_location_changed), page);
-#endif
   g_signal_connect (priv->search_entry, "map",
                     G_CALLBACK (entry_mapped), page);
   g_signal_connect (priv->map, "location-changed",

gnome-initial-setup/pages/timezone/gis-timezone-page.ui
@@ -28,7 +28,7 @@
             <property name="orientation">vertical</property>
             <property name="spacing">14</property>
             <child>
-              <object class="GtkEntry" id="search_entry">
+              <object class="GisLocationEntry" id="search_entry">
                 <property name="visible">True</property>
                 <property name="halign">center</property>
                 <property name="max-width-chars">55</property>

commit 8486fda8ed6232aa086855bebb3bfac5f33285f5

timezone: Load GisLocationEntry type before page template
Without this, constructing the timezone page fails and everything goes
south.

> (gnome-initial-setup:2071): Gtk-CRITICAL **: 18:06:35.097: Error
> building template class 'GisTimezonePage' for an instance of type
> 'GisTimezonePage': .:3:1233 Invalid object type 'GisLocationEntry'

Fixes: a7dfea2 "timezone: Reintroduce GisLocationEntry"
Part-of: <https://gitlab.gnome.org/GNOME/gnome-initial-setup/-/merge_requests/138>

gnome-initial-setup/pages/timezone/gis-timezone-page.c
@@ -522,6 +522,7 @@ gis_timezone_page_init (GisTimezonePage *page)
   g_type_ensure (CC_TYPE_TIMEZONE_MAP);
   g_type_ensure (GIS_TYPE_BUBBLE_WIDGET);
   g_type_ensure (GIS_TYPE_PAGE_HEADER);
+  g_type_ensure (GIS_TYPE_LOCATION_ENTRY);
 
   gtk_widget_init_template (GTK_WIDGET (page));
 }