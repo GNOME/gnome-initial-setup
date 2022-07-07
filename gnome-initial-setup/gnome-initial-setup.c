ae46a2b1ad98685424b21476eddb4ae71458bf66
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2022-07-07 09:12:44 -0300

    software: Port to GTK4
    A boring page to port.

--- gnome-initial-setup/gnome-initial-setup.c
+++ gnome-initial-setup/gnome-initial-setup.c
@@ -35,7 +35,7 @@
 #include "pages/network/gis-network-page.h"
 #include "pages/timezone/gis-timezone-page.h"
 #include "pages/privacy/gis-privacy-page.h"
-//#include "pages/software/gis-software-page.h"
+#include "pages/software/gis-software-page.h"
 //#include "pages/goa/gis-goa-page.h"
 //#include "pages/account/gis-account-pages.h"
 //#include "pages/parental-controls/gis-parental-controls-page.h"
@@ -68,7 +68,7 @@ static PageData page_table[] = {
   PAGE (network,  FALSE),
   PAGE (privacy,  FALSE),
   PAGE (timezone, TRUE),
-  //PAGE (software, TRUE),
+  PAGE (software, TRUE),
   //PAGE (goa,      FALSE),
   //PAGE (account,  TRUE),
   //PAGE (password, TRUE),
