a3c8593b8483e20afc669b57a0efd2aba6838f33
Will Thompson <wjt@endlessos.org>
2024-02-02 10:04:09 +0000

    privacy: Use G_DECLARE_FINAL_TYPE

--- gnome-initial-setup/pages/privacy/gis-privacy-page.h
+++ gnome-initial-setup/pages/privacy/gis-privacy-page.h
@@ -29,26 +29,7 @@
 G_BEGIN_DECLS
 
 #define GIS_TYPE_PRIVACY_PAGE               (gis_privacy_page_get_type ())
-#define GIS_PRIVACY_PAGE(obj)                           (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIS_TYPE_PRIVACY_PAGE, GisPrivacyPage))
-#define GIS_PRIVACY_PAGE_CLASS(klass)                   (G_TYPE_CHECK_CLASS_CAST ((klass),  GIS_TYPE_PRIVACY_PAGE, GisPrivacyPageClass))
-#define GIS_IS_PRIVACY_PAGE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIS_TYPE_PRIVACY_PAGE))
-#define GIS_IS_PRIVACY_PAGE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  GIS_TYPE_PRIVACY_PAGE))
-#define GIS_PRIVACY_PAGE_GET_CLASS(obj)                 (G_TYPE_INSTANCE_GET_CLASS ((obj),  GIS_TYPE_PRIVACY_PAGE, GisPrivacyPageClass))
-
-typedef struct _GisPrivacyPage        GisPrivacyPage;
-typedef struct _GisPrivacyPageClass   GisPrivacyPageClass;
-
-struct _GisPrivacyPage
-{
-  GisPage parent;
-};
-
-struct _GisPrivacyPageClass
-{
-  GisPageClass parent_class;
-};
-
-GType gis_privacy_page_get_type (void);
+G_DECLARE_FINAL_TYPE (GisPrivacyPage, gis_privacy_page, GIS, PRIVACY_PAGE, GisPage);
 
 GisPage *gis_prepare_privacy_page (GisDriver *driver);
 
