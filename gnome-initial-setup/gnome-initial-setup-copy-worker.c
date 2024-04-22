fca47a276814cfedecdfcf33f381f88c5ac38d62
Dan Nicholson <dbn@endlessos.org>
2024-04-22 09:33:05 -0600

    copy-worker: Always copy files
    Rather than moving the files from the gnome-initial-setup homedir, copy
    them. In practice the files will almost always be copied anyways since
    the g-i-s user's homedir is on a tmpfs in `/run` and the target user's
    homedir is most likely to be on real storage. Always making it a copy
    means it will behave the same way in all scenarios including testing.
    Moving the file also means the file is deleted from the g-i-s homedir,
    which just causes a bunch of journal spam during initial setup.

--- gnome-initial-setup/gnome-initial-setup-copy-worker.c
+++ gnome-initial-setup/gnome-initial-setup-copy-worker.c
@@ -44,7 +44,7 @@ file_is_ours (GFile *file)
 }
 
 static void
-move_file_from_homedir (GFile       *src_base,
+copy_file_from_homedir (GFile       *src_base,
                         GFile       *dest_base,
                         const gchar *path)
 {
@@ -56,11 +56,11 @@ move_file_from_homedir (GFile       *src_base,
 
   g_file_make_directory_with_parents (dest_parent, NULL, NULL);
 
-  g_debug ("Moving %s to %s", g_file_get_path (src), g_file_get_path (dest));
-  if (!g_file_move (src, dest, G_FILE_COPY_NONE,
+  g_debug ("Copying %s to %s", g_file_get_path (src), g_file_get_path (dest));
+  if (!g_file_copy (src, dest, G_FILE_COPY_NONE,
                     NULL, NULL, NULL, &error)) {
     if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND)) {
-      g_warning ("Unable to move %s to %s: %s",
+      g_warning ("Unable to copy %s to %s: %s",
                  g_file_get_path (src),
                  g_file_get_path (dest),
                  error->message);
@@ -99,7 +99,7 @@ main (int    argc,
   dest = g_file_new_for_path (g_get_home_dir ());
 
 #define FILE(path) \
-  move_file_from_homedir (src, dest, path);
+  copy_file_from_homedir (src, dest, path);
 
   FILE (".config/gnome-initial-setup-done");
   FILE (".config/dconf/user");
