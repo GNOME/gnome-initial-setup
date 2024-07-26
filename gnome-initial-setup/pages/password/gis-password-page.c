895ac639f71e9fa2412c3a6292a738c1ce4eb242
Will Thompson <wjt@endlessos.org>
2024-07-26 16:57:01 +0100

    password: Add password reminder
    When creating the user account, offers the user a way to hint what
    is the password, in case they don't remind it later.
    
    Based on an original patch by Alessandro Puccetti in Endless OS.
    Reworked by Philip Withnall to use `g_auto*` and make the reminder
    optional.  Reworked further by Will Thompson to use AdwEntryRow.
    
    See https://gitlab.gnome.org/GNOME/gnome-initial-setup/-/issues/56

--- gnome-initial-setup/pages/password/gis-password-page.c
+++ gnome-initial-setup/pages/password/gis-password-page.c
@@ -47,6 +47,7 @@ struct _GisPasswordPage
   GtkWidget *password_strength;
   GtkWidget *password_explanation;
   GtkWidget *confirm_explanation;
+  GtkWidget *reminder_entry;
   GtkWidget *header;
   GtkWidget *avatar;
 
@@ -182,10 +183,13 @@ gis_password_page_save_data (GisPage  *gis_page,
 
   password = gtk_editable_get_text (GTK_EDITABLE (page->password_entry));
 
-  if (strlen (password) == 0)
+  if (strlen (password) == 0) {
     act_user_set_password_mode (act_user, ACT_USER_PASSWORD_MODE_NONE);
-  else
-    act_user_set_password (act_user, password, "");
+  } else {
+    const gchar *reminder = gtk_editable_get_text (GTK_EDITABLE (page->reminder_entry));
+    g_autofree gchar *sanitized_reminder = g_strstrip (g_strdup (reminder));
+    act_user_set_password (act_user, password, sanitized_reminder);
+  }
 
   if (!page->parent_mode)
     gis_driver_set_user_permissions (gis_page->driver, act_user, password);
@@ -430,6 +434,7 @@ gis_password_page_class_init (GisPasswordPageClass *klass)
   gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (klass), GisPasswordPage, password_strength);
   gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (klass), GisPasswordPage, password_explanation);
   gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (klass), GisPasswordPage, confirm_explanation);
+  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (klass), GisPasswordPage, reminder_entry);
   gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (klass), GisPasswordPage, header);
   gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (klass), GisPasswordPage, avatar);
 
