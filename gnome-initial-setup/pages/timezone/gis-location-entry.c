9a9ad4ec637901f88d296df8fd39aa04f9b34a02
nmontero <nmontero@redhat.com>
2025-11-05 16:03:34 +0100

    timezone: Fix leak

--- gnome-initial-setup/pages/timezone/gis-location-entry.c
+++ gnome-initial-setup/pages/timezone/gis-location-entry.c
@@ -565,6 +565,7 @@ fill_location_entry_model (GtkListStore *store, GWeatherLocation *loc,
                                        show_named_timezones);
 
         g_free (display_name);
+        g_free (local_sort_name);
         g_free (local_compare_name);
         g_free (english_compare_name);
         break;
@@ -596,6 +597,7 @@ fill_location_entry_model (GtkListStore *store, GWeatherLocation *loc,
                                            -1);
 
         g_free (display_name);
+        g_free (local_sort_name);
         g_free (local_compare_name);
         g_free (english_compare_name);
         break;
